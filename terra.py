356e7f51e60fc9d4601f7ad88a19886b881240e7
xadhrit <maharajrabel@gmail.com>
2021-05-08 09:22:03 -0400

    driver file

--- terra.py
+++ terra.py
@@ -1,12 +1,15 @@
-from src.instagram import Instagram
 import argparse
 import sys
 import signal
 from rich.console import Console 
-from src.twitter import Twitter
+from src.twittr import Twitter
+from src.instagram import Instagram
 
 pc = Console()
 
+""" 
+First Check if os is windows or unix based
+"""
 
 is_windows = False
 
@@ -18,6 +21,9 @@ except:
 
     
 def main():
-    pc.print("  WELCOME TO THE MAIN MENU OF CLOSER:-)   ", style="yellow")
+    pc.print("   TERRA 2025   ", style="yellow")
 
+
+if __name__ == "__main__":
+    main()
 
