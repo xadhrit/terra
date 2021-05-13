6b944f2df1e0f4d21ea3d98727e3761d5f457440
xadhrit <maharajrabel@gmail.com>
2021-05-13 06:19:00 -0400

    insta module driver file

--- src/modules/insta.py
+++ src/modules/insta.py
@@ -1,4 +1,4 @@
-import sys
+import sys , os
 import signal
 from rich.console import Console
 import argparse
@@ -6,8 +6,8 @@ from instagram import Instagram
 
 
 pc = Console()
-is_wind = sys.platform.startswith('win')
 
+is_wind = os.name.startswith('win' or 'nt')
 
 if is_wind:
     import pyreadline
@@ -15,58 +15,69 @@ if is_wind:
 else:
     import gnureadline
     
-pc.print("Just Testing..", style="red")
+#pc.print("Just Testing..", style="red")
 
 
 def list_all_commands():
-    pc.print("FILE=y/n\t", style="yellow")
-    print("Enable/disable output in a '<target username>_<command>.txt' file'")
-    pc.print("JSON=y/n\t", style="white")
+    
+    pc.print("FILE=y/n : ", style="yellow",end='')
+    print(" Enable/disable output in a '<target username>_<command>.txt' file")
+    pc.print("JSON=y/n :  \t", style="white", end='')
     print("Enable/disable export in a '<target username>_<command>.json' file'")
-    pc.print("locations\t\t", style="green")
+    pc.print("locations :  \t", style="green", end='')
     print("Get all registered addressed by target photos")
-    pc.print("captions\t", style="cyan")
+    pc.print("captions : \t", style="cyan", end='')
     print("Get target's photos captions")
-    pc.print("comments\t", style="red")
+    pc.print("comments : \t", style="red", end='')
     print("Get total comments of target's posts")
-    pc.print("followers\t", style="yellow")
+    pc.print("followers : \t", style="yellow", end='')
     print("get target's followers")
-    pc.print("followings\t", style="red")
+    pc.print("followings : \t", style="red", end='')
     print("Get users followed by target")
-    pc.print("followers emails\t", style="green")
+    pc.print("followers emails :  \t", style="green", end='')
     print("Get email of target followers")
-    pc.print("following emails\t", style="yellow")
+    pc.print("following emails :  \t", style="yellow", end='')
     print("Get email of users followed by target")
-    pc.print("followers  phone\t", style="red")
+    pc.print("followers  phone :  \t", style="red", end='')
     print("Get phone number of target followers")
-    pc.print("followings phone\t" ,style="cyan")
+    pc.print("followings phone :  \t" ,style="cyan", end='')
     print("Get phone number of users followed by target")
-    pc.print("tags\t", style="yellow")
+    pc.print("tags : \t", style="yellow", end='')
     print("Get hashtags used by target")
-    pc.print("timeline\t\t", style="white")
+    pc.print("info : \t", style="white", end='')
     print("Target  timeline and information")
-    pc.print("Posts\t\t", style="green")
-    print("Get total number of posts of target.")
-    pc.print("likes\t\t", style="red")
+    pc.print("likes : \t", style="red", end='')
     print("Get total likes of target's posts")
-    pc.print("mediatype\t", style="green")
+    pc.print("mediatype :  \t", style="green", end='')
     print("Get target's posts type (photo or video)")
-    pc.print("photodes\t", style="cyan")
+    pc.print("photodes :  \t", style="cyan", end='')
     print("Get description of target's photos")
-    pc.print("photos\t\t", style="yellow")
+    pc.print("photos  : \t", style="yellow", end='')
     print("Download target's photos in output folder")
-    pc.print("profile pic\t\t", style="white")
+    pc.print("profile pic :  \t", style="white", end='')
     print("Download target's profile picture")
-    pc.print("stories\t\t", style="cyan")
+    pc.print("stories : \t", style="cyan", end='')
     print("Download target's stories")
-    pc.print("tagged\t\t", style="red")
+    pc.print("tagged  :  \t", style="red", end='')
     print("Get list of users tagged by target")
-    pc.print("reset target\t\t", style="white")
+    pc.print("reset target : \t\t", style="white", end='')
     print("Set new target")
-    pc.print("commenter\t", style="red")
+    pc.print("commenter : \t", style="red", end='')
     print("Get a list of user who commented target's photos")
-    pc.print("ttag\t\t", style="green")
+    pc.print("ttag  :  \t", style="green", end='')
     print("Get a list of user who tagged target")
+    print(" ")
+    pc.print(" Also supports basic terminal commands : ", style='cyan')
+    pc.print("ls : ", style='pink1', end='')
+    pc.print("Displaying all Commands ", ":search:", style='bright_white')
+    pc.print("exit : ", style='orange_red1', end='')
+    pc.print("For Exit from Terra", style='bright_white')
+    pc.print("clear: ", style='orchid2', end='')
+    pc.print("Clear your Screen", style='bright_white')
+    
+    
+    
+    
      
 def handle_single(sig, frame):
     pc.print("\nThroughing You Out\n", style="red")
@@ -81,10 +92,17 @@ def completer(text, state):
         return None
     
 def _out():
-    pc.print("Thank you for using Terra!", style="red")
+    pc.print("Thank you for using Terra!", style="bright_yellow")
     sys.exit(0)
     
-
+def clear():
+    if is_wind:
+        os.system('cls')
+        
+    else:
+        os.system('clear')
+        
+    
 signal.signal(signal.SIGINT, handle_single)
 
 if is_wind:
@@ -107,6 +125,7 @@ api = Instagram(args.id, args.file, args.json)
 commands = {
     'ls': list_all_commands,
     'help': list_all_commands,
+    'clear': clear,
     'quit' : quit,
     'exit': _out,
     'locations': api.target_locations,
@@ -137,7 +156,7 @@ gnureadline.parse_and_bind("tab : complete")
 gnureadline.set_completer(completer)
 
 while True:
-    pc.print("Terra Command : ", style="cyan")
+    pc.print("Terra Command : ", style="cyan", end='')
     user_input = input()
     
     cmd = commands.get(user_input)
