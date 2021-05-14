94dad89c2d0109056c763300315b816451b5f2b9
xadhrit <maharajrabel@gmail.com>
2021-05-14 03:14:19 -0400

    driver file

--- terra.py
+++ terra.py
@@ -7,22 +7,12 @@ from src.instagram import Instagram
 
 pc = Console()
 
-""" 
-First Check if os is windows or unix based
-"""
 
-is_windows = False
-
-try:
-    import gnureadline
-except:
-    is_windows = True
-    import pyreadline
 
     
 def main():
     pc.print("   TERRA 2025   ", style="yellow")
-
+    
 
 if __name__ == "__main__":
     main()
